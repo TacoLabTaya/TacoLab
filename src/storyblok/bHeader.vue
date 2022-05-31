5456dc7a9b946ca2091e88d9f55c55c06d463058
TacoLabTaya <taya@tacolab.tech>
2022-05-31 13:36:05 +0000

    Compornent

---
+++ src/storyblok/bHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+        <StoryblokComponent v-for="blok in blok.logo" :key="blok._uid" :blok="blok" />
+    </div>
+    <div  >
+      <StoryblokComponent v-for="blok in blok.navi" :key="blok._uid" :blok="blok" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object });
+</script>
---
+++ src/storyblok/eLink.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <span
+    v-editable="blok"
+  >
+    {{ blok.text }}
+  </span>
+</template>
+
+<script setup>
+defineProps({ blok: Object });
+</script>
