814c2d5c5e6b2dba7a64cd80ad1dc27c0babb2e6
TacoLabTaya <taya@tacolab.tech>
2022-05-31 11:46:56 +0000

    AddSetting

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    Header
+    <div v-for="blok in global.content.Header" :key="blok._uid" >
+        <StoryblokComponent :blok="blok" />
+    </div>
     <slot />
     Footer
   </div>
 </template>
+<script setup>
+const global = await useStoryblok('global', { version: 'published' });
+//console.log(global.content.Header[0]);
+</script>   
---
+++ src/storyblok/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      HEDOGA
+    <div v-for="blok in blok.NaviMenus" :key="blok._uid" >
+      <StoryblokComponent :blok="blok" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object });
+//console.log(blok);
+</script>
