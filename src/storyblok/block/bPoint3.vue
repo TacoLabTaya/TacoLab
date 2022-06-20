6ddd373db3e07668de16b93e69a31ea3f7a2c56a
TacoLabTaya <taya@tacolab.tech>
2022-06-20 10:30:22 +0000

    point

--- src/storyblok/block/bPoint3.vue
+++ src/storyblok/block/bPoint3.vue
@@ -5,11 +5,37 @@
 </template>
 
 <script setup>
-defineProps({ blok: Object });
+  const prop = defineProps({ blok: Object });
+  const stylePC = computed( () => {
+    return `${prop.blok.stylePC}`;
+  } );
+  const styleSP = computed( () => {
+    return `${prop.blok.stylePC}`;
+  } );
+  const height = computed( () => {
+    switch(prop.blok.height.style){
+      case 'percent':
+        return `${prop.blok.height.value}%`;
+      case 'vheight':
+        return `${prop.blok.height.value}vh`;
+      case 'auto':
+      default:
+        return 'auto';
+    }
+  } );
 </script>
 
 <style lang="scss" >
 .bPoint3{
+  height:v-bind(height);
   display:flex;
+  justify-content:space-around;
+  @include mq('LARGE'){
+    flex-direction:v-bind(stylePC);
+  }
+  @include mq('SHORT'){
+    flex-direction:v-bind(styleSP);
+  }
+  height:100%;
 }
 </style>
--- src/storyblok/element/eCard.vue
+++ src/storyblok/element/eCard.vue
@@ -1,17 +1,54 @@
 <template>
   <div class="eCard">
-    <h4 class="eCard-title">{{blok.title}}</h4>
-    <p  class="eCard-text" >{{blok.text }}</p>
+    <div class="eCard-title">
+      <StoryblokComponent v-for="blok in blok.title" :key="blok._uid" :blok="blok"/>
+    </div>
+    <div class="eCard-text">
+      <StoryblokComponent v-for="blok in blok.text" :key="blok._uid" :blok="blok"/>
+    </div>
+    <StoryblokComponent v-for="blok in blok.background" :key="blok._uid" :blok="blok" class="eCard-background"/>
   </div>
 </template>
 
 <script setup>
-defineProps({ blok: Object });
+  const prop = defineProps({ blok: Object });
+  const marginSP = computed( () => {
+    return`${prop.blok.spaces.margintsp}% ${prop.blok.spaces.marginrsp}% ${prop.blok.spaces.marginbsp}% ${prop.blok.spaces.marginlsp}%`;
+  } );
+  const paddingSP = computed( () => {
+    return `${prop.blok.spaces.paddingtsp}% ${prop.blok.spaces.paddingrsp}% ${prop.blok.spaces.paddingbsp}% ${prop.blok.spaces.paddinglsp}%`;
+  } );
+  const marginPC = computed( () => {
+    return `${prop.blok.spaces.margintpc}% ${prop.blok.spaces.marginrpc}% ${prop.blok.spaces.marginbpc}% ${prop.blok.spaces.marginlpc}%`;
+  } );
+  const paddingPC = computed( () => {
+   return `${prop.blok.spaces.paddingtpc}% ${prop.blok.spaces.paddingrpc}% ${prop.blok.spaces.paddingbpc}% ${prop.blok.spaces.paddinglpc}%`;
+  } );
 </script>
 
 <style lang="scss" >
 .eCard{
   .eCard-title{}
   .eCard-text {}
+  
+  @include mq('SHORT'){ 
+    margin :v-bind(marginSP);
+    padding:v-bind(paddingSP);
+  }
+  @include mq('LARGE'){ 
+    margin :v-bind(marginPC);
+    padding:v-bind(paddingPC);
+  }
+  
+  position:relative;
+  z-index: 0;
+  .eCard-title,.eCard-text{
+    z-index:1;
+  }
+  .eCard-background{
+    position:absolute;
+    z-index:-1;
+    top:0;right:0;bottom:0;left:0;
+  }
 }
 </style>
