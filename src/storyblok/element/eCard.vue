3291d71792e1338cd55009b2e188f6863e92458f
TacoLabTaya <taya@tacolab.tech>
2022-06-26 15:16:13 +0000

    before font awesome

--- src/storyblok/element/eCard.vue
+++ src/storyblok/element/eCard.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="eCard">
-    <div class="eCard-title">
-      <StoryblokComponent v-for="blok in blok.title" :key="blok._uid" :blok="blok"/>
+  <div :class="[
+      'eCard',
+      {'eCard-expand':blok.expand},
+      isOpen ? 'eCard-expand-open': 'eCard-expand-close',
+    ]">
+    <div class="eCard-top" @click="toggleSwitch" >
+      <div class="eCard-title">
+        <StoryblokComponent v-for="blok in blok.title" :key="blok._uid" :blok="blok"/>
+      </div>
+      <div v-if="isExpand" 
+        :class="[
+          'eCard-switch', 
+          blok.switch != null && blok.switch != ''? `eCard-switch-${blok.switch}`: 'eCard-switch-plus',
+        ]">
+        <span>+</span>
+        {{isOpen?'open':'close'}}
+      </div>
     </div>
-    <div class="eCard-text">
+    <div v-if="isOpen" class="eCard-text">
       <StoryblokComponent v-for="blok in blok.text" :key="blok._uid" :blok="blok"/>
     </div>
     <StoryblokComponent v-for="blok in blok.background" :key="blok._uid" :blok="blok" class="eCard-background"/>
@@ -13,28 +27,40 @@
 <script setup>
   const prop = defineProps({ blok: Object });
   const marginSP = computed( () => {
-    if( prop.blok.marginSP == null || prop.blok.marginSP.ison === false) return "0%";
-    return`${prop.blok.marginSP.top}% ${prop.blok.marginSP.right}% ${prop.blok.marginSP.bottom}% ${prop.blok.marginSP.left}%`;
+    if( prop.blok.marginSP == null || prop.blok.marginSP.ison === false) {return "0%"};
+    return `${prop.blok.marginSP.top}% ${prop.blok.marginSP.right}% ${prop.blok.marginSP.bottom}% ${prop.blok.marginSP.left}%`;
   } );
   const paddingSP = computed( () => {
-    if( prop.blok.paddingSP == null || prop.blok.paddingSP.ison === false) return "0%";
-    return`${prop.blok.paddingSP.top}% ${prop.blok.paddingSP.right}% ${prop.blok.paddingSP.bottom}% ${prop.blok.paddingSP.left}%`;
+    if( prop.blok.paddingSP == null || prop.blok.paddingSP.ison === false) {return "0%"};
+    return `${prop.blok.paddingSP.top}% ${prop.blok.paddingSP.right}% ${prop.blok.paddingSP.bottom}% ${prop.blok.paddingSP.left}%`;
   } );
   const marginPC = computed( () => {
-    if( prop.blok.marginPC == null || prop.blok.marginPC.ison === false) return "0%";
-    return`${prop.blok.marginPC.top}% ${prop.blok.marginPC.right}% ${prop.blok.marginPC.bottom}% ${prop.blok.marginPC.left}%`;
+    if( prop.blok.marginPC == null || prop.blok.marginPC.ison === false) {return "0%"};
+    return `${prop.blok.marginPC.top}% ${prop.blok.marginPC.right}% ${prop.blok.marginPC.bottom}% ${prop.blok.marginPC.left}%`;
   } );
   const paddingPC = computed( () => {
-    if( prop.blok.paddingPC == null || prop.blok.paddingPC.ison === false) return "0%";
-    return`${prop.blok.paddingPC.top}% ${prop.blok.paddingPC.right}% ${prop.blok.paddingPC.bottom}% ${prop.blok.paddingPC.left}%`;
+    if( prop.blok.paddingPC == null || prop.blok.paddingPC.ison === false) {return "0%"};
+    return `${prop.blok.paddingPC.top}% ${prop.blok.paddingPC.right}% ${prop.blok.paddingPC.bottom}% ${prop.blok.paddingPC.left}%`;
   } );
+
+  const isExpand = computed( () => {
+    if( prop.blok.expand == null ) return false;
+    return prop.blok.expand;
+  } );
+
+  const isOpen = ref(!isExpand);
+  const toggleSwitch = () => {
+    console.log(`toggle ${isExpand.value} ${isOpen.value}`);
+    if(isExpand.value) {isOpen.value = !isOpen.value};
+  };
+  const openSwitch = () => {
+    if(isExpand.value) {isOpen.value = ture};
+  };
 </script>
 
 <style lang="scss" >
+@import "@/assets/styles/_accordion.scss";
 .eCard{
-  .eCard-title{}
-  .eCard-text {}
-
   @include mq('SHORT'){ 
     margin :v-bind(marginSP);
     padding:v-bind(paddingSP);
@@ -44,15 +70,30 @@
     padding:v-bind(paddingPC);
   }
   
+
   position:relative;
   z-index: 0;
-  .eCard-title,.eCard-text{
+  .eCard-top{
     z-index:1;
+    display:flex;
+    .eCard-title { flex:1; }
+    .eCard-switch{ flex:0; }
+  }
+  .eCard-text {
+    z-index: 1;
   }
   .eCard-background{
     position:absolute;
     z-index:-1;
-    //top:0;right:0;bottom:0;left:0;
   }
+
+
+  .eCard-expand-open .eCard-switch-plus{
+    @include acco-bttn-plus--open();
+  }
+  .eCard-expand-close .eCard-switch-plus{
+    @include acco-bttn-plus--close();
+  }
+
 }
 </style>
