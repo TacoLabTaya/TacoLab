5d11b7bd762e1dfdfedd65edda9bffcf7517db6c
TacoLabTaya <taya@tacolab.tech>
2022-06-28 08:25:55 +0000

    update

--- src/storyblok/element/eCard.vue
+++ src/storyblok/element/eCard.vue
@@ -85,6 +85,7 @@
   }
   const setSwitch = (flag) => {
     if(isExpand.value) { isOpen.value = flag }
+    if(flag == false && rowHeight == 999) { rowHeight = getEleHigh(hide.value); }
     hide.value.style.maxHeight  = isOpen.value ? `${rowHeight*2}px` : 0;
     hide.value.style.margin     = isOpen.value ? null : 0;
   }
