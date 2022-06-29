766b63453ce2eab315b691e522d349639e32b685
TacoLabTaya <taya@tacolab.tech>
2022-06-29 06:30:47 +0000

    subpages

---
+++ src/pages/works/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+const story = await useStoryblok('works', { version: 'published' });
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
---
+++ src/storyblok/timeline/eTimeEvent.vue
@@ -0,0 +1,15 @@
+<template>
+  <picture class="eImage">
+    <source v-if="blok.sourcePC.id" :srcset="blok.sourcePC.filename" media="(min-width: 769px)" />
+    <img :src="blok.source.filename" />
+  </picture>
+</template>
+
+<script setup>
+defineProps({ blok: Object });
+</script>
+
+<style lang="scss" scoped>
+.eImage{
+}
+</style>
