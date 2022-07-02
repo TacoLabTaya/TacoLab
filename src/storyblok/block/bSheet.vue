e4f37c946042a9024cc4044f90c820f8e023810e
TacoLabTaya <taya@tacolab.tech>
2022-07-02 07:07:08 +0000

    column fr

--- src/storyblok/block/bSheet.vue
+++ src/storyblok/block/bSheet.vue
@@ -41,10 +41,10 @@
   const get4Position = (pos) => {
     if( pos == null || pos.ison === false) return "0%";
     const u = pos.unit == null ? '%' : pos.unit;
-    const valt = (pos.sett == null || pos.sett ) ? `${pos.top}${u}`   : 'auto' ;
-    const valr = (pos.setr == null || pos.setr ) ? `${pos.right}${u}` : 'auto';
-    const vall = (pos.setl == null || pos.setl ) ? `${pos.left}${u}`: 'auto';
-    const valb = (pos.setb == null || pos.setb ) ? `${pos.bottom}${u}`  : 'auto';
+    const valt = (pos.sett == null || !pos.sett ) ? 'auto': `${pos.top}${u}`;
+    const valr = (pos.setr == null || !pos.setr ) ? 'auto': `${pos.right}${u}`;
+    const vall = (pos.setl == null || !pos.setl ) ? 'auto': `${pos.left}${u}`;
+    const valb = (pos.setb == null || !pos.setb ) ? 'auto': `${pos.bottom}${u}`;
     return `${valt} ${valr} ${valb} ${vall}`;
   };
   const marginSP = computed( () => {
@@ -102,12 +102,19 @@
   const columnNumberTB = computed( () => { return columnNumber.value.tb });
   const columnNumberPC = computed( () => { return columnNumber.value.pc });
   const columnNumberXL = computed( () => { return columnNumber.value.xl });
-  /*
-  const calcSpan = (column) => {
-    return column === 1 ? 1 
-  }
-  */
 
+  const fr = '1fr ';
+  const gridColumnsXS = computed(  () =>  { return fr.repeat(columnNumberXS.value) });
+  const gridColumnsSP = computed(  () =>  { return fr.repeat(columnNumberSP.value) });
+  const gridColumnsTB = computed(  () =>  { return fr.repeat(columnNumberTB.value) });
+  const gridColumnsPC = computed(  () =>  { return fr.repeat(columnNumberPC.value) });
+  const gridColumnsXL = computed(  () =>  { return fr.repeat(columnNumberXL.value) });
+
+  const itemWidthXS = computed(  () =>  { return `${100 / columnNumberXS.value }%`;});
+  const itemWidthSP = computed(  () =>  { return `${100 / columnNumberSP.value }%`;});
+  const itemWidthTB = computed(  () =>  { return `${100 / columnNumberTB.value }%`;});
+  const itemWidthPC = computed(  () =>  { return `${100 / columnNumberPC.value }%`;});
+  const itemWidthXL = computed(  () =>  { return `${100 / columnNumberXL.value }%`;});
 
 </script>
 
@@ -149,23 +156,26 @@
   &.mSheet-layout-gMasonry,&.mSheet-layout-gMasonryRow,&.mSheet-layout-gTile{
     display:grid;
     grid-gap:v-bind(gridGap);
-    @include mq('xs'){ grid-template-columns:repeat(v-bind(columnNumberXS),1fr); }
-    @include mq('sp'){ grid-template-columns:repeat(v-bind(columnNumberSP),1fr); }
-    @include mq('tb'){ grid-template-columns:repeat(v-bind(columnNumberTB),1fr); }
-    @include mq('pc'){ grid-template-columns:repeat(v-bind(columnNumberPC),1fr); }
-    @include mq('xl'){ grid-template-columns:repeat(v-bind(columnNumberXL),1fr); }
+    /*
+    @include mq('xs'){ grid-template-columns:v-bind(gridColumnsXS); }
+    @include mq('sp'){ grid-template-columns:v-bind(gridColumnsSP); }
+    @include mq('tb'){ grid-template-columns:v-bind(gridColumnsTB); }
+    @include mq('pc'){ grid-template-columns:v-bind(gridColumnsPC); }
+    @include mq('xl'){ grid-template-columns:v-bind(gridColumnsXL); }
+    */
+    //grid-template-columns:repeat(auto-fit,minmax(100px,1fr));
   }
   &.mSheet-layout-gMasonry{
     //grid-template-columns: repeat(auto-fit, minmax(33%, 1fr));
     grid-auto-rows: v-bind(rowHeight);
-    //grid-auto-flow: row dense;
+    grid-auto-flow: row dense;
     @for $i from 1 through 99 {
       >*:nth-child(#{$i}){
-        @include mq('xs'){ grid-row: #{$i} / span v-bind(columnNumberXS); }
-        @include mq('sp'){ grid-row: #{$i} / span v-bind(columnNumberSP); }
-        @include mq('tb'){ grid-row: #{$i} / span v-bind(columnNumberTB); }
-        @include mq('pc'){ grid-row: #{$i} / span v-bind(columnNumberPC); }
-        @include mq('xl'){ grid-row: #{$i} / span v-bind(columnNumberXL);; }
+        @include mq('xs'){ grid-row: #{$i} / span v-bind(columnNumberXS);}
+        @include mq('sp'){ grid-row: #{$i} / span v-bind(columnNumberSP);}
+        @include mq('tb'){ grid-row: #{$i} / span v-bind(columnNumberTB);}
+        @include mq('pc'){ grid-row: #{$i} / span v-bind(columnNumberPC);}
+        @include mq('xl'){ grid-row: #{$i} / span v-bind(columnNumberXL);}
       }
     }
   }
@@ -175,6 +185,15 @@
     justify-items  : v-bind(justifyItems);  
     align-content  : v-bind(alignContent);  
     align-items    : v-bind(alignItems);  
+    /*
+    >*{
+        @include mq('xs'){ width:v-bind(itemWidthXS);}
+        @include mq('sp'){ width:v-bind(itemWidthSP);}
+        @include mq('tb'){ width:v-bind(itemWidthTB);}
+        @include mq('pc'){ width:v-bind(itemWidthPC);}
+        @include mq('xl'){ width:v-bind(itemWidthXL);}
+    }
+    */
   }
 }
 </style>
